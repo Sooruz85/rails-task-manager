2aff5f8d6523b4b13bdb292e4362684b1aeee459
etienne gaumery <gaumery@hotmail.com>
2024-03-26 09:15:14 +0100

    rails new

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,37 @@
 class TasksController < ApplicationController
+
+  def task
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+
+    if @task.save
+      redirect_to task_path(@task), notice: 'Task was successfully created.'
+    else
+    render :new
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    if @task.update(task_params)
+      redirect_to task_path(@task), notice: 'Task was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,5 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  get '/tasks', to: 'tasks#index'
   # root "posts#index"
-  
 end
